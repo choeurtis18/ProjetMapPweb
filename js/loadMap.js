502f99820e04f5cbeada87a419da44af1f7f0df1
Choeurtis <choeurtis.tchounga@gmail.com>
2021-03-21 17:05:05 +0100

    Update of PoPub

--- js/loadMap.js
+++ js/loadMap.js
@@ -23,16 +23,16 @@ $(document).ready(function(){
         [45.4695797,    4.4502821,  'Auvergne-Rhone-Alpes'],
         [47.280513,     4.999437,   'Bourgogne-Franche-Comte'],
         [48.202047,     -2.932644,  'Bretagne'],
-        [47.499998,     1.749997,   'Centre-Val de Loire'],
+        [47.499998,     1.749997,   'Centre-Val-de-Loire'],
         [42.200604,     9.092893,   'Corse'],
-        [48.580002,     5.950000,   'Grand Est'],
-        [50.047503,     2.363062,   'Haut de france'],
+        [48.580002,     5.950000,   'Grand-Est'],
+        [50.047503,     2.363062,   'Haut-de-france'],
         [48.999870,     0.171253,   'Normandie'],
         [48.7099198,    2.6000411,  'Ile-De-France'],
         [45.399044,     0.2995785,   'Nouvelle-Aquitaine'],
         [43.545911,     1.998862,    'Occitanie'],
-        [47.7632836,    -0.3299687, 'Pays de la loire'],
-        [43.9351691,    6.0679194,  "Provence-Alpes-Cote D'Azur"] 
+        [47.7632836,    -0.3299687, 'Pays-de-la-loire'],
+        [43.9351691,    6.0679194,  "Provence-Alpes-Cote-D-Azur"] 
     ];
 
     /* On boucle sur le tableau pour y placer les points */
